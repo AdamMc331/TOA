6f34c24c584c4239f09764bd43f8229b36020f9a
adammc331 <amcneilly331@gmail.com>
2021-09-22 19:09:53 -0400

    Adding color palette for #19.

--- app/src/main/java/com/adammcneilly/toa/ui/theme/Color.kt
+++ app/src/main/java/com/adammcneilly/toa/ui/theme/Color.kt
@@ -2,9 +2,21 @@
 
 package com.adammcneilly.toa.ui.theme
 
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+private val PrimaryBlue = Color(0xFF0A3751)
+private val SecondaryOrange = Color(0xFFFC8138)
+private val BackgroundBlue = Color(0xFFF4F4F8)
+private val OnBackgroundBlack = Color(0xFF282828)
+
+val lightColorPalette = lightColors(
+    primary = PrimaryBlue,
+    onPrimary = Color.White,
+    secondary = SecondaryOrange,
+    onSecondary = Color.White,
+    background = BackgroundBlue,
+    onBackground = OnBackgroundBlack,
+    surface = Color.White,
+    onSurface = PrimaryBlue,
+)
