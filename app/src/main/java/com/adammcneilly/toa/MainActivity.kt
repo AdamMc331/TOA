79a2b347d494018c01932fbea6cc58a7ca0945c3
adammc331 <amcneilly331@gmail.com>
2021-09-22 19:11:59 -0400

    Updating stream-history.md for #19.

--- app/src/main/java/com/adammcneilly/toa/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/toa/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TOATheme {
+            TOATheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -32,7 +32,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TOATheme {
+    TOATheme() {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/adammcneilly/toa/ui/theme/Theme.kt
+++ app/src/main/java/com/adammcneilly/toa/ui/theme/Theme.kt
@@ -1,13 +1,11 @@
 package com.adammcneilly.toa.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TOATheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = lightColorPalette
 
