6d3e64c968c7568f1d7601259954366ed3c7a61a
adammc331 <amcneilly331@gmail.com>
2021-10-13 21:59:22 -0400

    Using single Urbanist family.

--- app/src/main/java/com/adammcneilly/toa/core/ui/theme/Type.kt
+++ app/src/main/java/com/adammcneilly/toa/core/ui/theme/Type.kt
@@ -3,15 +3,25 @@ package com.adammcneilly.toa.core.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import com.adammcneilly.toa.R
 
-private val UrbanistExtraBold = FontFamily(Font(R.font.urbanist_extrabold))
-private val UrbanistSemiBold = FontFamily(Font(R.font.urbanist_semibold))
-private val UrbanistBold = FontFamily(Font(R.font.urbanist_bold))
-private val UrbanistMedium = FontFamily(Font(R.font.urbanist_medium))
-private val UrbanistLight = FontFamily(Font(R.font.urbanist_light))
-private val UrbanistRegular = FontFamily(Font(R.font.urbanist_regular))
+private val UrbanistExtraBold = Font(R.font.urbanist_extrabold, FontWeight.ExtraBold)
+private val UrbanistSemiBold = Font(R.font.urbanist_semibold, FontWeight.SemiBold)
+private val UrbanistBold = Font(R.font.urbanist_bold, FontWeight.Bold)
+private val UrbanistMedium = Font(R.font.urbanist_medium, FontWeight.Medium)
+private val UrbanistLight = Font(R.font.urbanist_light, FontWeight.Light)
+private val UrbanistRegular = Font(R.font.urbanist_regular, FontWeight.Normal)
+
+private val UrbanistFamily = FontFamily(
+    UrbanistExtraBold,
+    UrbanistSemiBold,
+    UrbanistBold,
+    UrbanistMedium,
+    UrbanistLight,
+    UrbanistRegular,
+)
 
 val typography = Typography(
-    defaultFontFamily = UrbanistRegular,
+    defaultFontFamily = UrbanistFamily,
 )
