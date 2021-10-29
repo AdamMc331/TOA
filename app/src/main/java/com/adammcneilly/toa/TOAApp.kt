1e6434e6bccc8ad43c76f09de700ca9a8f2be232
adammc331 <amcneilly331@gmail.com>
2021-10-28 21:52:38 -0400

    Using Dynamic Theming.

--- app/src/main/java/com/adammcneilly/toa/TOAApp.kt
+++ app/src/main/java/com/adammcneilly/toa/TOAApp.kt
@@ -1,7 +1,15 @@
 package com.adammcneilly.toa
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TOAApp : Application()
+class TOAApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
--- app/src/main/java/com/adammcneilly/toa/core/ui/theme/Theme.kt
+++ app/src/main/java/com/adammcneilly/toa/core/ui/theme/Theme.kt
@@ -1,18 +1,27 @@
 package com.adammcneilly.toa.core.ui.theme
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.os.BuildCompat
 
+@RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun TOATheme(
     isDark: Boolean = isSystemInDarkTheme(),
+    dynamic: Boolean = BuildCompat.isAtLeastS(),
     content: @Composable () -> Unit,
 ) {
-    val colors = if (isDark) {
-        DarkThemeColors
-    } else {
-        LightThemeColors
+    val colors = when {
+        dynamic && isDark -> dynamicDarkColorScheme(LocalContext.current)
+        dynamic && !isDark -> dynamicLightColorScheme(LocalContext.current)
+        isDark -> DarkThemeColors
+        else -> LightThemeColors
     }
 
     MaterialTheme(
