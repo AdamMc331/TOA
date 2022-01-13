515e681b16fc81396fb366298db1095523294bb0
adammc331 <amcneilly331@gmail.com>
2022-01-12 20:18:51 -0500

    Testing UseCase for input validation.

--- app/src/main/java/com/adammcneilly/toa/addtask/domain/usecases/ProdAddTaskUseCase.kt
+++ app/src/main/java/com/adammcneilly/toa/addtask/domain/usecases/ProdAddTaskUseCase.kt
@@ -4,6 +4,7 @@ import com.adammcneilly.toa.addtask.domain.model.AddTaskResult
 import com.adammcneilly.toa.core.data.Result
 import com.adammcneilly.toa.tasklist.domain.model.Task
 import com.adammcneilly.toa.tasklist.domain.repository.TaskListRepository
+import java.time.LocalDate
 import javax.inject.Inject
 
 class ProdAddTaskUseCase @Inject constructor(
@@ -11,11 +12,31 @@ class ProdAddTaskUseCase @Inject constructor(
 ) : AddTaskUseCase {
 
     override suspend fun invoke(task: Task): AddTaskResult {
+        val validationResult = validateInput(task)
+
+        if (validationResult != null) {
+            return validationResult
+        }
+
         val result = taskListRepository.addTask(task)
 
         return when (result) {
             is Result.Success -> AddTaskResult.Success
             is Result.Error -> AddTaskResult.Failure.Unknown
+        }
+    }
+
+    private fun validateInput(task: Task): AddTaskResult.Failure.InvalidInput? {
+        val emptyDescription = task.description.isEmpty()
+        val scheduledDateInPast = task.scheduledDate.isBefore(LocalDate.now())
+
+        return if (emptyDescription || scheduledDateInPast) {
+            AddTaskResult.Failure.InvalidInput(
+                emptyDescription = emptyDescription,
+                scheduledDateInPast = scheduledDateInPast,
+            )
+        } else {
+            null
         }
     }
 }
