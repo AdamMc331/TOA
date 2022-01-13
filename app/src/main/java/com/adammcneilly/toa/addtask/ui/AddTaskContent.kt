4f0c930f4879ba795a05bfc332c833c9b4831152
adammc331 <amcneilly331@gmail.com>
2022-01-12 20:24:10 -0500

    Showing error message when submitting empty description.

--- app/src/main/java/com/adammcneilly/toa/addtask/ui/AddTaskContent.kt
+++ app/src/main/java/com/adammcneilly/toa/addtask/ui/AddTaskContent.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,6 +84,9 @@ private fun AddTaskInputsColumn(
             text = viewState.taskInput.description,
             onTextChanged = onTaskDescriptionChanged,
             enabled = viewState.inputsEnabled,
+            errorMessage = (viewState as? AddTaskViewState.Active)
+                ?.descriptionInputErrorMessage
+                ?.getString(),
         )
 
         TaskDateLabel()
@@ -151,6 +155,7 @@ private fun TaskDescriptionInput(
     text: String,
     onTextChanged: (String) -> Unit,
     enabled: Boolean,
+    errorMessage: String?,
 ) {
     TOATextField(
         text = text,
@@ -160,7 +165,8 @@ private fun TaskDescriptionInput(
         keyboardOptions = KeyboardOptions.Default.copy(
             capitalization = KeyboardCapitalization.Sentences,
         ),
-        placeholderText = "Clean my office space...",
+        placeholderText = stringResource(R.string.task_input_placeholder),
+        errorMessage = errorMessage,
     )
 }
 
