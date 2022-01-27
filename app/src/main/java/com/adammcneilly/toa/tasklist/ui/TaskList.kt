e52f7f56a67476be7558beac8697c5e66cd5e69f
adammc331 <amcneilly331@gmail.com>
2022-01-26 20:48:17 -0500

    Style cleanup, ignoring magic number in previews.

--- app/src/main/java/com/adammcneilly/toa/tasklist/ui/TaskList.kt
+++ app/src/main/java/com/adammcneilly/toa/tasklist/ui/TaskList.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.adammcneilly.toa.R
 import com.adammcneilly.toa.core.ui.theme.TOATheme
@@ -29,9 +31,7 @@ fun TaskList(
         modifier = modifier,
     ) {
         item {
-            Text(
-                text = "Incomplete Tasks",
-            )
+            SectionHeader(text = stringResource(R.string.incomplete_tasks_header))
         }
 
         items(incompleteTasks) { task ->
@@ -47,9 +47,7 @@ fun TaskList(
         }
 
         item {
-            Text(
-                text = "Completed Tasks",
-            )
+            SectionHeader(text = stringResource(R.string.completed_tasks_header))
         }
 
         items(completedTasks) { task ->
@@ -64,6 +62,16 @@ fun TaskList(
             )
         }
     }
+}
+
+@Composable
+private fun SectionHeader(
+    text: String,
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.headlineSmall,
+    )
 }
 
 @Preview(
