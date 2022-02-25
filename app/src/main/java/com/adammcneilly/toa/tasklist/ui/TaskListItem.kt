cd1ded0d6d4511262e4ecedb88a5942aa7631405
adammc331 <amcneilly331@gmail.com>
2022-02-24 19:22:48 -0500

    Removing buttons from completed tasks as part of #99.

--- app/src/main/java/com/adammcneilly/toa/tasklist/ui/TaskListItem.kt
+++ app/src/main/java/com/adammcneilly/toa/tasklist/ui/TaskListItem.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.toa.R
 import com.adammcneilly.toa.core.models.Task
@@ -42,10 +44,12 @@ fun TaskListItem(
                 task.description,
             )
 
-            ButtonRow(
-                onRescheduleClicked = onRescheduleClicked,
-                onDoneClicked = onDoneClicked,
-            )
+            if (!task.completed) {
+                ButtonRow(
+                    onRescheduleClicked = onRescheduleClicked,
+                    onDoneClicked = onDoneClicked,
+                )
+            }
         }
     }
 }
@@ -96,6 +100,28 @@ private fun TaskText(
     )
 }
 
+private class TaskPreviewParameterProvider : PreviewParameterProvider<Task> {
+
+    override val values: Sequence<Task>
+        get() {
+            val incompleteTask = Task(
+                id = "test",
+                description = "Clean my office space.",
+                scheduledDateMillis = 0L,
+                completed = false,
+            )
+
+            val completedTask = incompleteTask.copy(
+                completed = true,
+            )
+
+            return sequenceOf(
+                incompleteTask,
+                completedTask,
+            )
+        }
+}
+
 @Preview(
     name = "Night Mode",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
@@ -106,14 +132,10 @@ private fun TaskText(
 )
 @Composable
 @Suppress("UnusedPrivateMember")
-private fun TaskListItemPreview() {
-    val task = Task(
-        id = "test",
-        description = "Clean my office space.",
-        scheduledDateMillis = 0L,
-        completed = false,
-    )
-
+private fun TaskListItemPreview(
+    @PreviewParameter(TaskPreviewParameterProvider::class)
+    task: Task,
+) {
     TOATheme {
         TaskListItem(
             task = task,
