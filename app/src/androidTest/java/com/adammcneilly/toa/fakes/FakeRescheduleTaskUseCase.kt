ac4a0f6dae4118f48707d69408a1c3f23b217770
adammc331 <amcneilly331@gmail.com>
2022-04-09 15:07:01 -0400

    Updating tests.

---
+++ app/src/androidTest/java/com/adammcneilly/toa/fakes/FakeRescheduleTaskUseCase.kt
@@ -0,0 +1,18 @@
+package com.adammcneilly.toa.fakes
+
+import com.adammcneilly.toa.core.models.Task
+import com.adammcneilly.toa.tasklist.domain.usecases.RescheduleTaskUseCase
+import com.google.common.truth.Truth.assertThat
+import java.time.LocalDate
+
+class FakeRescheduleTaskUseCase : RescheduleTaskUseCase {
+    private val invocations: MutableList<Pair<Task, LocalDate>> = mutableListOf()
+
+    override suspend fun invoke(task: Task, newDate: LocalDate) {
+        invocations.add(Pair(task, newDate))
+    }
+
+    fun verifyInvocation(task: Task, newDate: LocalDate) {
+        assertThat(invocations).contains(Pair(task, newDate))
+    }
+}
