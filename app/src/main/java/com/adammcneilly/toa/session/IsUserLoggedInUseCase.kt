c056c28de952bc44eafb7a22c0319cf7e1e57c33
adammc331 <amcneilly331@gmail.com>
2022-06-22 19:54:42 -0400

    Conditional logic to navigate to first screen.

--- app/src/main/java/com/adammcneilly/toa/session/IsUserLoggedInUseCase.kt
+++ app/src/main/java/com/adammcneilly/toa/session/IsUserLoggedInUseCase.kt
@@ -1,12 +1,13 @@
 package com.adammcneilly.toa.session
 
 import com.adammcneilly.toa.login.domain.repository.TokenRepository
+import javax.inject.Inject
 
 /**
  * A use case to extract the domain logic to check if a user is currently signed in
  * on this device.
  */
-class IsUserLoggedInUseCase(
+class IsUserLoggedInUseCase @Inject constructor(
     private val tokenRepository: TokenRepository
 ) {
 
