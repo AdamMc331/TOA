2c112f89353de94acf0515b3ac9ce0168edec0d7
Adam McNeilly <amcneilly331@gmail.com>
2023-02-22 19:53:09 -0500

    Conditionally showing bottom bar.

--- app/src/main/java/com/adammcneilly/toa/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/toa/MainActivity.kt
@@ -12,15 +12,20 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.FragmentActivity
 import com.adammcneilly.toa.core.ui.WindowSize
+import com.adammcneilly.toa.core.ui.components.navigation.NavigationTab
+import com.adammcneilly.toa.core.ui.components.navigation.NavigationTabDisplayModel
+import com.adammcneilly.toa.core.ui.components.navigation.TOABottomNavigation
 import com.adammcneilly.toa.core.ui.rememberWindowSizeClass
 import com.adammcneilly.toa.core.ui.theme.TOATheme
 import com.adammcneilly.toa.destinations.LoginScreenDestination
@@ -36,6 +41,7 @@ import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultA
 import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.manualcomposablecalls.composable
 import com.ramcosta.composedestinations.spec.Route
+import com.ramcosta.composedestinations.utils.currentDestinationAsState
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalAnimationApi
@@ -83,28 +89,59 @@ class MainActivity : FragmentActivity() {
         startRoute: Route,
         windowSize: WindowSize,
     ) {
-        DestinationsNavHost(
-            startRoute = startRoute,
-            navGraph = NavGraphs.root,
-            engine = rememberAnimatedNavHostEngine(
-                rootDefaultAnimations = RootNavGraphDefaultAnimations(
-                    enterTransition = {
-                        slideInHorizontally()
-                    },
-                    exitTransition = {
-                        fadeOut()
-                    },
-                ),
+        val navigationEngine = rememberAnimatedNavHostEngine(
+            rootDefaultAnimations = RootNavGraphDefaultAnimations(
+                enterTransition = {
+                    slideInHorizontally()
+                },
+                exitTransition = {
+                    fadeOut()
+                },
             ),
-            manualComposableCallsBuilder = {
-                composable(TaskListScreenDestination) {
-                    TaskListScreen(
-                        navigator = destinationsNavigator,
-                        windowSize = windowSize,
-                    )
-                }
-            }
         )
+
+        val navController = navigationEngine.rememberNavController()
+
+        Column {
+            DestinationsNavHost(
+                startRoute = startRoute,
+                navGraph = NavGraphs.root,
+                engine = navigationEngine,
+                navController = navController,
+                manualComposableCallsBuilder = {
+                    composable(TaskListScreenDestination) {
+                        TaskListScreen(
+                            navigator = destinationsNavigator,
+                            windowSize = windowSize,
+                        )
+                    }
+                },
+                modifier = Modifier
+                    .weight(1F),
+            )
+
+            val shouldShowBottomBar = when (navController.currentDestinationAsState().value?.route) {
+                TaskListScreenDestination.route -> true
+                else -> false
+            }
+
+            if (shouldShowBottomBar) {
+                val tabs = listOf(
+                    NavigationTabDisplayModel(
+                        tab = NavigationTab.Home,
+                        isSelected = true,
+                        onClick = {},
+                    ),
+                    NavigationTabDisplayModel(
+                        tab = NavigationTab.Settings,
+                        isSelected = false,
+                        onClick = {},
+                    ),
+                )
+
+                TOABottomNavigation(navigationTabs = tabs)
+            }
+        }
     }
 
     private fun keepSplashScreenVisibleWhileInitializing() {
