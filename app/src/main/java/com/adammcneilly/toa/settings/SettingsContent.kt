602050b26a17907742a8202729754c0f8a6525e0
Adam McNeilly <amcneilly331@gmail.com>
2023-02-24 12:33:51 -0500

    Adding textfield & switch.

--- app/src/main/java/com/adammcneilly/toa/settings/SettingsContent.kt
+++ app/src/main/java/com/adammcneilly/toa/settings/SettingsContent.kt
@@ -1,26 +1,24 @@
 package com.adammcneilly.toa.settings
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.adammcneilly.toa.R
 
@@ -43,71 +41,30 @@ fun SettingsContent(
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
-                text = "Preferred number of tasks per day:",
+                text = "Number Of Tasks Per Day",
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .weight(1F),
             )
 
-            Dropdown()
-        }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun Dropdown() {
-    val exposedDropdown = remember { mutableStateOf(false) }
-    val preferredTasks = remember { mutableStateOf("N/A") }
-
-    ExposedDropdownMenuBox(
-        expanded = exposedDropdown.value,
-        onExpandedChange = { expanded ->
-            exposedDropdown.value = expanded
-        },
-    ) {
-        BasicTextField(
-            value = preferredTasks.value,
-            onValueChange = {},
-            readOnly = true,
-            modifier = Modifier
-                .border(
-                    width = 1.dp,
-                    color = MaterialTheme.colorScheme.primary,
-                )
-                .menuAnchor()
-                .padding(horizontal = 16.dp)
-                .width(IntrinsicSize.Min),
-            textStyle = MaterialTheme.typography.bodyMedium.copy(
-                color = LocalContentColor.current,
-            ),
-        )
-
-        ExposedDropdownMenu(
-            expanded = exposedDropdown.value,
-            onDismissRequest = {
-                exposedDropdown.value = false
-            },
-            modifier = Modifier
-                .exposedDropdownSize(),
-        ) {
-            DropdownMenuItem(
-                text = { Text("N/A") },
-                onClick = {
-                    preferredTasks.value = "N/A"
-                },
+            Switch(
+                checked = true,
+                onCheckedChange = {},
             )
-
-            for (i in 1..5) {
-                DropdownMenuItem(
-                    text = { Text(i.toString()) },
-                    onClick = {
-                        preferredTasks.value = i.toString()
-                        exposedDropdown.value = false
-                    },
-                )
-            }
         }
+
+        OutlinedTextField(
+            value = "3",
+            onValueChange = {},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+            ),
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = CircleShape,
+        )
     }
 }
