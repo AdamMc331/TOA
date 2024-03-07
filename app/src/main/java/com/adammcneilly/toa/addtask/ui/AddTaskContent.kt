c74e7bc15cce20e7e440b486e8ba85a990444d95
Adam McNeilly <amcneilly331@gmail.com>
2024-03-06 22:29:03 -0500

    Formatting fixes.

--- app/src/main/java/com/adammcneilly/toa/addtask/ui/AddTaskContent.kt
+++ app/src/main/java/com/adammcneilly/toa/addtask/ui/AddTaskContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
